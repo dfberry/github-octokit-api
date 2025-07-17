3727bf8dbce5e97517c9da1715382f93674bc07e
dfberry <dinaberry@outlook.com>
2025-07-17 13:08:03 +0000

    template - slight clean up

--- packages/create-document-template-data/src/index.ts
+++ packages/create-document-template-data/src/index.ts
@@ -10,7 +10,6 @@ import {
 import { getCurrentGithubDbPath } from '@dfb/finddb';
 
 import path from 'path';
-import { promises as fs } from 'fs';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
